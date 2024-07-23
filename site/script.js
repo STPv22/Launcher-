c7f0964e76abadca439c2d5dcc3ecb60d0420524
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-07-23 16:17:20 +0000

    add banner image from zentic

--- site/script.js
+++ site/script.js
@@ -5,7 +5,8 @@ splashtext.innerHTML = "loading..."
 
 function init() {
     //update body styles
-    document.body.style.overflow = "scroll";
+    document.body.style.overflowX = "hidden";
+    document.body.style.overflowY = "scroll";
     document.body.style.backgroundColor = "rgb(50, 50, 50)";
     document.body.style.left = "20%";
 
@@ -18,12 +19,17 @@ function init() {
         background-color: rgb(24, 24, 24);
         border: none;`;
     
-    var something = "s";
+    var bannerImage = document.createElement("img");
+    bannerImage.src = "./assets/banner.png";
+    bannerImage.style = `
+        width: 100%;
+        `;
 
     //testing
     document.addEventListener("DOMContentLoaded", (event) => {
         console.log("DOM fully loaded and parsed");
 
+        document.body.appendChild(bannerImage);
         document.body.appendChild(sideBar);
     });
 }
