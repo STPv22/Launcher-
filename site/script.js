390df73d597b0190ab201f515a48af6201a8893e
Stpv22 <145866689+STPv22@users.noreply.github.com>
2024-07-23 15:52:18 +0000

    more setup

--- site/script.js
+++ site/script.js
@@ -2,10 +2,40 @@ var splashtext = document.getElementById("loadingSplashText");
 splashtext.innerHTML = "loading..."
 
 
-function init() {
-    var sideBar = document.createElement("div");
 
+function init() {
+    //update body styles
+    document.body.style.overflow = "scroll";
+    document.body.style.backgroundColor = "rgb(50, 50, 50)";
+    document.body.style.left = "20%";
+
+    //create elements for launcher
+    var sideBar = document.createElement("div");
+    sideBar.style = `
+        position: fixed;
+        left: 0;
+        top: 0;
+        background-color: rgb(24, 24, 24);
+        border: none;`;
+
+    //testing
     document.addEventListener("DOMContentLoaded", (event) => {
         console.log("DOM fully loaded and parsed");
+
+        document.body.appendChild(sideBar);
     });
 }
+
+function clientLaunch() {
+    if (getIfClientInstalled()) {
+
+    } else {
+
+    }
+}
+
+function getIfClientInstalled() {
+    return false;
+}
+
+init();
